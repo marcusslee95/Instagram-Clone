5230a89920d812f91ed3052119e281816902ec7a
Marcus Lee <marcusslee95@berkeley.edu>
2021-03-01 23:07:58 -0800

    i start up server after pool connection to db gets established. I don't want situation where I start my server before pool connection to db gets established s.t. someone can send server a request to an endpoint that's supposed to do some db operation but can't because pool hasn't established connection to db yet

--- node-api/src/index.js
+++ node-api/src/index.js
@@ -2,14 +2,13 @@ const pool = require('./pool')
 const createsAndConfiguresServer = require('./app')
 
 
-createsAndConfiguresServer().listen(3333, () => {
-    console.log('Yo API is UP!')
-})
-
 pool
 .query('SELECT * FROM comments WHERE id = 404') //random query to test that pool can interact w/db
 .then((res) => {
     console.log('Yo Thing Being Used to interact w/db - aka pool - is working! \nSee here is a column value of the row we are getting back -> ' + res.rows[0].contents)
+    createsAndConfiguresServer().listen(3333, () => {
+        console.log('Yo server is up & Listening for requests!')
+    })
 })
 .catch((err) => console.error(err))
 
